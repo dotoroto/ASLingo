e3dd6c6899576189456de1bc4886a7a24e5cd291
lily <lilyyou56@gmail.com>
2025-10-04 07:50:13 -0400

    Homepage updated again

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -26,6 +26,11 @@ export default function Homepage() {
       <div className="centered-title">
         <h1>WANT TO LEARN ASL?</h1>
       </div>
+      <div className="centered-text">
+        <p>Jump into <strong>ASLingo</strong>, the gamified way to learn <br />
+        <strong>American Sign Language!</strong> Get instant feedback, crush mini-games, <br />
+        and level up your skills. <strong>No matter your level, this is your all-in-one ASL platform.</strong></p>
+      </div>
       <div className="homepage-buttons">
          <button className="frontpage-btn" onClick={() => navigate("/login")}>Login</button>
          <button className="frontpage-btn" onClick={() => navigate("/signup")}>Sign Up</button>
