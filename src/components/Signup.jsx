dd6a54effd8a9cc782e442b13327aa7bce6b927b
randomhoomanbean <dorothy.zheng07@gmail.com>
2025-10-05 08:16:40 -0400

    fixed signup

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -7,6 +7,7 @@ import { Link } from "react-router-dom";
 import { useTranslation } from "react-i18next";
 
 export default function Signup() {
+  const [name, setName] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState("");
