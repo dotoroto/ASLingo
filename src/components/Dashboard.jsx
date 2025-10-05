013e8321eadf286547ea98a2f46471282676b8fc
lily <lilyyou56@gmail.com>
2025-10-04 21:39:28 -0400

    new avatar

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import logo from "../assets/logo.png";
-import avatar from "../assets/avatarimage.jpg"
+import avatar from "../assets/avatarimage.png"
 import { useNavigate } from "react-router-dom";
 import { Link } from 'react-router-dom';
 import Navbar from './Navbar';
